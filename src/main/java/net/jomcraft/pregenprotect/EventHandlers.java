546457b793dc497b299c7b7373ca8e216e994d21
PT400C Jomcraft <pt400c@gmail.com>
2021-08-27 21:43:08 +0200

    Simple serial process

--- src/main/java/net/jomcraft/pregenprotect/EventHandlers.java
+++ src/main/java/net/jomcraft/pregenprotect/EventHandlers.java
@@ -8,45 +8,6 @@ import pregenerator.common.manager.ServerManager;
 
 public class EventHandlers {
 	
-	/*public static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-	
-	//public static volatile MinecraftServer server = null; 
-	
-	//public static volatile boolean end = false;
-	
-	static {
-		executor.scheduleAtFixedRate(() -> {
-			
-			if(Thread.interrupted())
-				return;
-			MinecraftServer l = ServerLifecycleHooks.getCurrentServer();
-		if(!l.isRunning())
-			return;
-		
-			//if(server != null) {
-			//synchronized (server) {
-			//ServerManager.INSTANCE.pauseTask(null, null);
-					//PreGenProtect.log.error("asdsadasd " + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024));
-			
-			try {
-			int ram = (int) ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024);
-			
-			if(ram > 12000) {
-				if(ServerManager.INSTANCE.isRunning())
-					ServerManager.INSTANCE.pauseTask(null, x -> {});
-				ServerLifecycleHooks.getCurrentServer().halt(false);
-			}
-			PreGenProtect.log.error(ram);
-			} catch (Exception e) {
-				PreGenProtect.log.error(e);
-			}
-					//
-			
-				//}
-			////}
-		}, 60, 30, TimeUnit.SECONDS);
-	}*/
-	
 	@SubscribeEvent
 	public void onRenderTick(TickEvent.ServerTickEvent event) {
 		if (event.phase == Phase.END && ServerLifecycleHooks.getCurrentServer().getTickCount() % 600 == 0) {
@@ -64,27 +25,5 @@ public class EventHandlers {
 		}
 
 	}
-	
-	/*
-
-	
-
-	@SubscribeEvent
-	public void serverStarting(FMLServerStartingEvent event) {
-		
-		//synchronized (server) {
-			server = ((MinecraftServer) LogicalSidedProvider.INSTANCE.get(LogicalSide.SERVER));
-		//}
-	}
-	
-	@SubscribeEvent
-	public void serverStarting(FMLServerStoppingEvent event) {
-		
-		//synchronized (server) {
-		//end = true;
-			executor.shutdownNow();
-		//}
-	}*/
-
 	
 }
--- src/main/java/net/jomcraft/pregenprotect/PreGenProtect.java
+++ src/main/java/net/jomcraft/pregenprotect/PreGenProtect.java
@@ -8,9 +8,6 @@ import com.electronwill.nightconfig.core.CommentedConfig;
 import com.electronwill.nightconfig.toml.TomlParser;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(value = PreGenProtect.MODID)
 public class PreGenProtect {
@@ -19,36 +16,21 @@ public class PreGenProtect {
 	public static final Logger log = LogManager.getLogger(PreGenProtect.MODID);
 	public static final String VERSION = getModVersion();
 	public static PreGenProtect instance;
-	
+
 	public PreGenProtect() {
 		instance = this;
 
-			MinecraftForge.EVENT_BUS.register(PreGenProtect.class);
-
-			MinecraftForge.EVENT_BUS.register(new EventHandlers());
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-			FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup2);
-		
+		MinecraftForge.EVENT_BUS.register(PreGenProtect.class);
+		MinecraftForge.EVENT_BUS.register(new EventHandlers());
 	}
-	
+
 	public static PreGenProtect getInstance() {
 		return instance;
 	}
 
-	private void clientSetup(FMLClientSetupEvent event) {
-	
-	}
-	
-	
-	
-	private void clientSetup2(FMLCommonSetupEvent event) {
-		
-	}
-
-	
 	@SuppressWarnings("unchecked")
 	public static String getModVersion() {
-		//Stupid FG 3 workaround
+		// Stupid FG 3 workaround
 		TomlParser parser = new TomlParser();
 		InputStream stream = PreGenProtect.class.getClassLoader().getResourceAsStream("META-INF/mods.toml");
 		CommentedConfig file = parser.parse(stream);
